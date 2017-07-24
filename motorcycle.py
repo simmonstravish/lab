b7337889521de7cabeac10f3373f816113c0cb36
simmonstravish <simmonstravish@gmail.com>
2017-07-23 21:35:01 -0500

    Create motorcycle.py

---
+++ motorcycle.py
@@ -0,0 +1,19 @@
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+message = "Hello Python Crash course reader!"
+print(message)
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
